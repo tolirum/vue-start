19eb23dbc44633b1ac29396d3922fe11d5ac3499
Maxim Maznov <tolirum@gmail.com>
2021-07-13 17:23:50 +0600

    Vue start app

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="round">
+      <img alt="Vue logo" src="./assets/logo.png" />
+    </div>
+    <Max msg="Frontend Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Max from "./components/Max.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Max,
+  },
+};
 </script>
 
 <style>
+body {
+  background-color: #e1f5fe;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +29,28 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.round {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  /* padding: 50px; */
+  margin: 0 auto;
+}
+
+.round:hover {
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+}
+
+img {
+  transition: 0.5s linear;
+  align-items: center;
+  margin: 20px;
 }
 </style>
---
+++ src/components/Max.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>{{ now }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Max",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      now: new Date(),
+    };
+  },
+  methods: {
+    updateDate() {
+      this.now = new Date();
+    },
+  },
+  mounted() {
+    setInterval(() => {
+      this.updateDate();
+      console.log("Max, you are Genius!!!");
+    }, 1000);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+
+p {
+  color: #2b7c58;
+}
+
+h1,
+p {
+  text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.14), 0 2px 1px rgba(0, 0, 0, 0.12),
+    0 1px 3px rgba(0, 0, 0, 0.2);
+}
+
+h1:hover,
+p:hover {
+  text-shadow: 1px 2px 2px rgba(0, 0, 0, 0.14), 0 3px 1px rgba(0, 0, 0, 0.12),
+    0 1px 5px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
